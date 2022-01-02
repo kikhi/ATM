4b71e1b8455d4c11909e5768919e620e14b4df41
Cesar Trujillo <catg20g@gmail.com>
2022-01-01 21:08:04 -0800

    Agregue query de la base de datos

--- Program.cs
+++ Program.cs
@@ -1,63 +1,84 @@
-﻿Console.WriteLine("Bienvenido al cajero de criptomonedas! \n");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
 
-Console.WriteLine("1.- Depositar");
-Console.WriteLine("2.- Enviar");
-Console.WriteLine("3.- Retirar");
-
-int menu = Convert.ToInt32(Console.ReadLine());
-
-switch(menu) 
+namespace Cajero
 {
-  case 1:
-    Depositar();
-    break;
-  case 2:
-    Enviar();
-    break;
-  case 3:
-    Retirar();
-    break;
-  default:
-    Console.WriteLine("Opcion no valida");
-    break;
-}
-
-static void Depositar(){
-    int menu;
-    int deposito;
-    Dinero xrp = new Dinero();
-
-    Console.Clear();
-    Console.WriteLine("Que moneda deseas depositar");
-    Console.WriteLine("1.- xrp");
-    Console.WriteLine("2.- etherium");
-    Console.WriteLine("3.- bitcoin");
-    menu = Convert.ToInt32(Console.ReadLine());
-
-    switch(menu)
+    public class Program
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al cajero de criptomonedas! \n");
+
+            Console.WriteLine("1.- Depositar");
+            Console.WriteLine("2.- Enviar");
+            Console.WriteLine("3.- Retirar");
+
+            int menu = Convert.ToInt32(Console.ReadLine());
+
+            switch(menu) 
+            {
+              case 1:
+              Depositar();
+                break;
+              case 2:
+              Enviar();
+                break;
+              case 3:
+              Retirar();
+                break;
+              default:
+                Console.WriteLine("Opcion no valida");
+                break;
+            }
+        }
+
+      static void Depositar()
+      {
+        int menu;
+        int deposito;
+        Dinero xrp = new Dinero();
+
+        Console.Clear();
+        Console.WriteLine("Que moneda deseas depositar");
+        Console.WriteLine("1.- xrp");
+        Console.WriteLine("2.- etherium");
+        Console.WriteLine("3.- bitcoin");
+        menu = Convert.ToInt32(Console.ReadLine());
+
+        switch(menu)
+        {
         case 1:
             Console.WriteLine("Ingresa el monto a depositar");
             deposito = Convert.ToInt32(Console.ReadLine());
             //xrp = deposito + xrp;
             break;
+        }
+      }
+
+      static void Enviar()
+      {
+        Console.Write("Enviar");
+      }
+
+      static void Retirar()
+      {
+        Console.WriteLine("Retirar");
+      }
+
+      static void conexionDB()
+      {
+      SqlConnection connectionDB = new SqlConnection("SERVER=CLIENTE\SQLEXPRESS; DATABASE=criptos; integrated security=true");
+      }
     }
-}
-
-static void Enviar(){
-    Console.Write("Enviar");
-}
-
-static void Retirar(){
-    Console.WriteLine("Retirar");
-}
-
-class Dinero
-{
-    int dolares;
-    int xrp;
-    int etherium;
-    int bitcoin;
 
 
+    class Dinero
+    {
+      int dolares;
+      int xrp;
+      int etherium;
+      int bitcoin;
+    }
 }
