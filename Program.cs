dd2604d03d77453a129a3c2ba8462436830c90e1
Cesar Trujillo <catg20g@gmail.com>
2022-02-24 20:03:20 -0800

    ATM csharp

--- Program.cs
+++ Program.cs
@@ -13,11 +13,10 @@ namespace Cajero
         public static void Main(string[] args)
         {
           /*    ==== Menu ====    */
-            Console.WriteLine("Bienvenido al cajero de criptomonedas! \n");
+            Console.WriteLine("Criptos ATM\n");
             Console.WriteLine("1.- Depositar");
-            Console.WriteLine("2.- Enviar");
-            Console.WriteLine("3.- Retirar");
-            Console.WriteLine("4 - caso de pruebas");
+            Console.WriteLine("2.- Withdraw");
+            Console.WriteLine("3.- Transfer");
 
             int menu = Convert.ToInt32(Console.ReadLine());
 
@@ -39,24 +38,12 @@ namespace Cajero
             Console.ReadKey();
         }
 
-      static void crearBilletera()
-      {
-        string back = "R.- Regresar";
-
-        Console.WriteLine("Crea una billetera de criptomonedas ! \n");
-        Console.WriteLine("{0} \n1.- Ingresa tu nombre o nombres: ", back);
-        Console.WriteLine("{0} \n2.- Ingresa apellido paterno:", back);
-        Console.WriteLine("{0} \n3.- Ingresa apellido materno:", back);
-        Console.WriteLine("{0} \n3.- Crea un nup unico:", back);
-      }
-
       static void Depositar()
       {
         SqlConnection connectionDB = new SqlConnection("SERVER=CLIENTE\\SQLEXPRESS; DATABASE=criptos; integrated security=true");
         SqlCommand comand = new SqlCommand();
         string query;
         int menu;
-        //int cantidad;
 
         Console.Clear();
         Console.WriteLine("Que moneda desea depositar?");
@@ -68,51 +55,51 @@ namespace Cajero
 
         switch(menu)
         {
-          case 1:
+          case 1:    // XRP
             connectionDB.Open();
             string? xrpTxt = null;
 
             Console.Write("Insert xrp mount: ");
             xrpTxt = Console.ReadLine();
 
-            query = "UPDATE billetera SET xrp = '"+ xrpTxt +"' WHERE id_billetera = '00001';";
+            query = "UPDATE billetera SET xrp = xrp + '"+ xrpTxt +"' WHERE id_billetera = '00001';";
             comand = new SqlCommand(query, connectionDB);
             // comand.Parameters.AddWithValue("@xrp", xrpTxt);
             comand.ExecuteNonQuery();
             connectionDB.Close();
               break;
-          case 2:
+          case 2:    // ETHERIUM
             connectionDB.Open();
             string? etherium = null;
 
             Console.Write("Insert etherium mount: ");
             etherium = Console.ReadLine();
 
-            query = "UPDATE billetera SET etherium = '"+ etherium +"' WHERE id_billetera = '00001';";
+            query = "UPDATE billetera SET etherium = etherium + '"+ etherium +"' WHERE id_billetera = '00001';";
             comand = new SqlCommand(query, connectionDB);
             comand.ExecuteNonQuery();
             connectionDB.Close();
               break;
-          case 3:
+          case 3:    // BITCOIN
             connectionDB.Open();
             string? bitcoin = null;
 
             Console.Write("Insert bitcoin mount: ");
             bitcoin = Console.ReadLine();
 
-            query = "UPDATE billetera SET bitcoin = '"+ bitcoin +"' WHERE id_billetera = '00001';";
+            query = "UPDATE billetera SET bitcoin = bitcoin + '"+ bitcoin +"' WHERE id_billetera = '00001';";
             comand = new SqlCommand(query, connectionDB);
             comand.ExecuteNonQuery();
             connectionDB.Close();
               break;
-          case 4:
+          case 4:    // DOLLARS
             connectionDB.Open();
             string? dollars = null;
 
             Console.Write("Insert dollars mount: ");
             dollars = Console.ReadLine();
 
-            query = "UPDATE billetera SET dollars = '"+ dollars +"' WHERE id_billetera = '00001';";
+            query = "UPDATE billetera SET dollars = dollars + '"+ dollars +"' WHERE id_billetera = '00001';";
             comand = new SqlCommand(query, connectionDB);
             comand.ExecuteNonQuery();
             connectionDB.Close();
@@ -122,8 +109,10 @@ namespace Cajero
 
       static void Whithdraw()
       {
+        SqlConnection connectionDB = new SqlConnection("SERVER=CLIENTE\\SQLEXPRESS; DATABASE=criptos; integrated security=true");
+        SqlCommand comand = new SqlCommand();
+        string query;
         int menu;
-        int cantidad;
 
         Console.Clear();
         Console.WriteLine("Que moneda desea transferir?");
@@ -135,25 +124,54 @@ namespace Cajero
 
         switch(menu)
         {
-          case 1:
-            Console.WriteLine("Ingrese cantidad de xrp a transferir");
-            cantidad = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Deposito exitoso");
+          case 1:    // XRP
+            connectionDB.Open();
+            string? xrpTxt = null;
+
+            Console.Write("Insert xrp mount: ");
+            xrpTxt = Console.ReadLine();
+
+            query = "UPDATE billetera SET xrp = xrp - '"+ xrpTxt +"' WHERE id_billetera = '00001';";
+            comand = new SqlCommand(query, connectionDB);
+            // comand.Parameters.AddWithValue("@xrp", xrpTxt);
+            comand.ExecuteNonQuery();
+            connectionDB.Close();
               break;
-          case 2:
-            Console.WriteLine("Ingrese cantidad de etherium a transferir");
-            cantidad = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Deposito exitoso");
+          case 2:    // ETHERIUM
+            connectionDB.Open();
+            string? etherium = null;
+
+            Console.Write("Insert etherium mount: ");
+            etherium = Console.ReadLine();
+
+            query = "UPDATE billetera SET etherium = etherium - '"+ etherium +"' WHERE id_billetera = '00001';";
+            comand = new SqlCommand(query, connectionDB);
+            comand.ExecuteNonQuery();
+            connectionDB.Close();
               break;
-          case 3:
-            Console.WriteLine("Ingrese cantidad de bitcoin a transferir");
-            cantidad = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Deposito exitoso");
+          case 3:    // BITCOIN
+            connectionDB.Open();
+            string? bitcoin = null;
+
+            Console.Write("Insert bitcoin mount: ");
+            bitcoin = Console.ReadLine();
+
+            query = "UPDATE billetera SET bitcoin = bitcoin - '"+ bitcoin +"' WHERE id_billetera = '00001';";
+            comand = new SqlCommand(query, connectionDB);
+            comand.ExecuteNonQuery();
+            connectionDB.Close();
               break;
-          case 4:
-            Console.WriteLine("Ingrese cantidad de dolares a ingresar");
-            cantidad = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Deposito exitoso");
+          case 4:    // DOLLARS
+            connectionDB.Open();
+            string? dollars = null;
+
+            Console.Write("Insert dollars mount: ");
+            dollars = Console.ReadLine();
+
+            query = "UPDATE billetera SET dollars = dollars - '"+ dollars +"' WHERE id_billetera = '00001';";
+            comand = new SqlCommand(query, connectionDB);
+            comand.ExecuteNonQuery();
+            connectionDB.Close();
               break;
         }
       }
